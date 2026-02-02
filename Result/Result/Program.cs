255d2bfbce5ae0e4c7b2c057e90e51b58bd2f833
Meenal Patro <meenalpatro@gmail.com>
2026-02-02 18:42:07 +0530

    Add files via upload

---
+++ Result/Result/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Result
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name : Meenal Patro");
+            Console.WriteLine("Enrollment Number : 92400527259");
+            Console.WriteLine("Course : BCA");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Class : 1D");
+            Console.WriteLine("Subject Maths : O");
+			Console.WriteLine("Subject AOC : O");
+			Console.WriteLine("Subject Enterpreneurship : A+");
+			Console.WriteLine("Subject ES : A");
+			Console.WriteLine("Subject BWD : O");
+			Console.WriteLine("Subject PP1 : A+");
+            Console.WriteLine("Result : Pass");
+            Console.WriteLine("SGPA : 9.20");
+			Console.WriteLine("---------------------------------------------");
+			Console.WriteLine("Class : 2D");
+			Console.WriteLine("Subject Maths : O");
+			Console.WriteLine("Subject ADS : O");
+			Console.WriteLine("Subject Accounts : A+");
+			Console.WriteLine("Subject CS : O");
+			Console.WriteLine("Subject DBMS1 : O");
+			Console.WriteLine("Subject PP2 : O");
+			Console.WriteLine("Result : Pass");
+			Console.WriteLine("SGPA : 9.85");
+			Console.WriteLine("---------------------------------------------");
+			Console.WriteLine("Class : 3E");
+			Console.WriteLine("Subject MIS : O");
+			Console.WriteLine("Subject DBMS2 : O");
+			Console.WriteLine("Subject Java : O");
+			Console.WriteLine("Subject LS : A");
+			Console.WriteLine("Subject SIP : A");
+			Console.WriteLine("Subject BB : O");
+			Console.WriteLine("Result : Pass");
+			Console.WriteLine("SGPA : 9.43");
+
+		}
+    }
+}
