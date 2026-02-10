74809980f1f7082a2b78ad3a30cfe0f10c5b17dd
Meenal Patro <meenalpatro@gmail.com>
2026-02-10 08:42:00 +0530

    Add files via upload

---
+++ LoginPage/LoginPage/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace login
+{
+	class LoginApp
+	{
+		static void Main()
+		{
+			string correctusername = "Meenal";
+			string correctpassword = "0327";
+
+			int attempts = 3;
+
+			while (attempts > 0)
+			{
+				Console.Write("Enter Username: ");
+				string username = Console.ReadLine();
+
+				Console.Write("Enter Password: ");
+				string password = Console.ReadLine();
+
+				if (username == correctusername && password == correctpassword)
+				{
+					Console.WriteLine("\nLogin Successful!");
+					break;
+				}
+				else
+				{
+					attempts--;
+					Console.WriteLine("\nInvalid credentials!");
+
+					if (attempts > 0)
+					{
+						Console.WriteLine("Remaining attempts: " + attempts + "\n");
+					}
+					else
+					{
+						Console.WriteLine("Account locked. Too many failed attempts.");
+					}
+				}
+			}
+
+			Console.ReadLine(); 
+		}
+	}
+}
