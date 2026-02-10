67777a0647e43a1e9beb95254dd9397ec6e6f673
Meenal Patro <meenalpatro@gmail.com>
2026-02-10 07:58:42 +0530

    Add files via upload

---
+++ OddEven/OddEven/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OddEven
+{
+     class Program
+     {
+        static void Main(string[] args)
+        {
+			int a = int.Parse(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Number is even");
+            }
+            else 
+            {
+                Console.WriteLine("Invalid number");
+            }
+
+		}
+	 }
+}
