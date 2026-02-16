02fbe598c81780165edc46f5acfd414d2a6d34ae
Meenal Patro <meenalpatro@gmail.com>
2026-02-16 09:42:28 +0530

    Add files via upload

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Factorial
+{
+	class FactorialProgram
+	{
+		static void Main()
+		{
+			int n;
+			int fact = 1;
+
+			Console.WriteLine("Enter a number: ");
+			n = int.Parse(Console.ReadLine());
+
+			for (int i = 1; i <= n; i++)
+			{
+				fact = fact * i;
+			}
+
+			Console.WriteLine("Factorial of is: " + fact);
+		}
+	}
+}
