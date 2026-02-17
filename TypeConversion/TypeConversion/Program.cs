fdb553bab4483d55a350abf9fdcd7b0a61c433b7
Meenal Patro <meenalpatro@gmail.com>
2026-02-17 08:38:05 +0530

    Add files via upload

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,21 @@
+﻿//WAP to create two variables with int and float datatype and perform automatically conversion and forced conversion.
+using System;
+
+namespace TypeConversion
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			float a;
+			int b = 27;
+			a = b;
+			Console.WriteLine(a);
+
+			float x = 2000.0F;
+			int y;
+			y = (int)x;
+			Console.WriteLine(y);
+		}
+	}
+}
